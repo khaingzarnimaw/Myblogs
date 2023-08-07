3801c923d4042a97d0a468610fe7c98b0722ad73
kznm <khaingmaw@Khaings-MacBook-Air.local>
2023-08-07 15:38:38 +0900

    first step

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <nav>
+        <h1>My Blogs</h1>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
